371ebc311c6cd31657ed3bdc669f6f52ca1f848a
Ryan Eschinger <ryanesc@gmail.com>
2015-04-29 22:24:28 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 sudo apt-get install curl git python-pip python-dev
 sudo pip install -U ansible
-sudo /usr/local/bin/ansible-pull -i localhost, -U https://github.com/ryane/ansible-ryane-cloud-bootstrap.git
+/usr/local/bin/ansible-pull -i localhost, -U https://github.com/ryane/ansible-ryane-cloud-bootstrap.git -K
